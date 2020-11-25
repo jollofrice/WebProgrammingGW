d20c4ef218af8b15eb1855520a3d0808fbfab2a4
sendelivery <69419807+sendelivery@users.noreply.github.com>
2020-11-25 18:29:18 +0000

    B1 - Complete, small change to views.py
    form.isValid() is deprecated, so I switched it to the correct form.is_valid().
    
    Also changed return redirect('user profile') to return render(request, 'profile.html')
    
    I don't think you can use the urls.py name shortcut in your view functions, so instead I put the actual html file name.
    
    Finally, got rid of the commented out code.

--- TheDailySquid/tdsapp/views.py
+++ TheDailySquid/tdsapp/views.py
@@ -8,28 +8,22 @@ from django.shortcuts import render, redirect
 def index(request):
     return render(request, 'index.html')
 
-#def login(request):
- #   form = AuthenticationForm(request.POST or None)
-    #if request.method == 'POST':
-        #if form.isValid():
-    #    username = form.cleaned_data.get('username')
-    #    raw_password = form.cleaned_dat.get('password1')
-    #    user = authenticate(username = username, password = raw_password)
-    #    return redirect('user profile')
-    #else:
-  #  return render(request, 'login.html', {'form': form})
-
 def signup(request):
     form = UserCreationForm(request.POST or None)
+
     if request.method == 'POST':
-        if form.isValid():
+        if form.is_valid():
             #form.save()
             #username = form.cleaned_data.get('username')
             #raw_password = form.cleaned_dat.get('password1')
             #user = authenticate(username = username, password = raw_password)
             user = form.save()
             login(request, user)
-            return redirect('user profile')
+            return render(request, 'profile.html')
+
+        else:
+            return render(request, 'signup.html', {'form': form})
+
     else:
         return render(request, 'signup.html', {'form' : form})
 
